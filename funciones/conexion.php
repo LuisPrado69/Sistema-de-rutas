820503777587eb6eac4eddd003bef47df3ceb56a
LUCHO\LUCHO <luis6969uchiha@outlook.com>
2018-02-21 11:43:57 -0500

    no message

---
+++ funciones/conexion.php
@@ -0,0 +1,8 @@
+<?php
+	//conexion
+	$host='localhost';
+	$user='root';
+	$pass='Sistemas12.';
+	$db='integrador';
+	$con=mysqli_connect($host,$user,$pass,$db) or die ('<div class="error">Error al conectar con la base de datos</div>');
+?>
---
+++ header/header.php
@@ -0,0 +1,19 @@
+<table border="0">
+	<tr>
+		<td style="width: 150px; text-align:left;">
+		<a href="usuario.php" class="btn btn-primary">Chat</a>
+		</td>
+		<td style="width: 700px;">
+		</td>
+		<td style="width: 300px; text-align:right;">
+			<form action="agregar_contactos.php" method="post" name="buscar" id="buscar">	
+				<div class="col-xs-12">
+				    <input type="search" name="busqueda" id="busqueda" placeholder="Ingrese el nombre del usuario" class="form-control">
+				</div>
+		</td>
+		<td style="width: 80px; text-align:right;">		
+    		<input type="submit" name="buscar" id="buscar" value="Buscar" class="btn btn-info">
+		</form>
+		</td>
+	</tr>
+</table>
